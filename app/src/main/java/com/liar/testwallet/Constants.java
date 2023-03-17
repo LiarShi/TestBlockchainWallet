55fd01e0b174afee4f92984c7278d25564203dd2
Shi <526277580@qq.com>
2023-03-17 20:29:21 +0800

    去掉apkkey

--- app/src/main/java/com/liar/testwallet/Constants.java
+++ app/src/main/java/com/liar/testwallet/Constants.java
@@ -11,10 +11,10 @@ public class Constants {
     public static final String LIA_ADDRESS = "0x5a1E5F219e9a510A15c601a904AA1d940240E87a";
 
     /**GOERLI请求地址**/
-    public static final String ETHEREUM_GOERLI_URL="https://goerli.infura.io/v3/5a3e5d655f8642e18f775f639471cb4a";
+    public static final String ETHEREUM_GOERLI_URL="https://goerli.infura.io/v3/你的APKkey";
     /**MAINNET请求地址**/
-    public static final String ETHEREUM_MAINNET_URL="https://mainnet.infura.io/v3/5a3e5d655f8642e18f775f639471cb4a";
+    public static final String ETHEREUM_MAINNET_URL="https://mainnet.infura.io/v3/你的APKkey";
     /**SEPOLIA请求地址**/
-    public static final String ETHEREUM_SEPOLIA_URL="https://sepolia.infura.io/v3/5a3e5d655f8642e18f775f639471cb4a";
+    public static final String ETHEREUM_SEPOLIA_URL="https://sepolia.infura.io/v3/你的APKkey";
 
 }
